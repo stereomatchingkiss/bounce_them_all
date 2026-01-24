5676005566abc09ce88177757d1375010809e3d5
stereomatchingkiss <thamngapwei@gmail.com>
2026-01-24 23:54:48 +0800

    first commit

---
+++ characters/player/sound_manager_player.gd
@@ -0,0 +1,17 @@
+extends Node
+class_name SoundManagerPlayer
+
+func play_dead_player():
+	%dead_player.play(0.6)
+	
+func play_hit_enemy_by_arrow():
+	%hit_enemy_by_arrow.play(0.13)
+
+func play_jump():
+	%jump.play()
+	
+func play_land():
+	%land.play()
+	
+func play_shoot_standard_bullet():
+	%shoot_standard_bullet.play()
