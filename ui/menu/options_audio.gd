57eb4849ee99f22cc799abe230acb7601e5feae4
stereomatchingkiss <thamngapwei@gmail.com>
2026-01-26 09:33:23 +0800

    refactor the name of the nodes

--- ui/menu/options_audio.gd
+++ ui/menu/options_audio.gd
@@ -3,8 +3,8 @@ extends Control
 @onready var id_music_volume_ := AudioServer.get_bus_index("Music")
 @onready var id_sound_volume_ := AudioServer.get_bus_index("Sound")
 
-@onready var slider_music_volume: HSlider = %slider_music_volume
-@onready var slider_sound_volume: HSlider = %slider_sound_volume
+@onready var slider_music_volume_: HSlider = %SliderMusicVolume
+@onready var slider_sound_volume_: HSlider = %SliderSoundVolume
 
 func _on_slider_sound_volume_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_linear(id_sound_volume_, value)
@@ -15,8 +15,8 @@ func _on_slider_music_volume_value_changed(value: float) -> void:
 func _on_button_reset_defaults_pressed() -> void:
 	AudioServer.set_bus_volume_linear(id_music_volume_, 1.0)
 	AudioServer.set_bus_volume_linear(id_sound_volume_, 1.0)
-	slider_music_volume.value = 1.0
-	slider_sound_volume.value = 1.0
+	slider_music_volume_.value = 1.0
+	slider_sound_volume_.value = 1.0
 
 func _on_button_back_to_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://ui/menu/options_menu.tscn")
