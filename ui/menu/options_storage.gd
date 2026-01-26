61af30badbce050a578bbf03c13b2fa7f6bb3ef9
stereomatchingkiss <thamngapwei@gmail.com>
2026-01-26 08:20:27 +0800

    first commit

---
+++ ui/menu/options_storage.gd
@@ -0,0 +1,31 @@
+extends Resource
+class_name OptionsStorage
+
+enum Language{
+	English,
+	Chinese
+}
+
+enum Resolution{
+	R1920x1080,
+	R1152x648
+}
+
+enum MSAA{
+	M0,
+	M2,
+	M4,
+	M8
+}
+
+# Game
+var language := Language.English
+
+# Video
+var full_screen := true
+var msaa := MSAA.M2
+var resolution := Resolution.R1920x1080
+
+# Audio
+var music_volume := 1.0
+var sound_volume := 1.0
